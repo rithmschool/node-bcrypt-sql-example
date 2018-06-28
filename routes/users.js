2ac4362bc933108cf34b7a168b4940ae3610f6bc
Elie Schoppik <eschoppik@gmail.com>
2018-06-28 16:58:29 -0700

    adding authorization route

--- routes/users.js
+++ routes/users.js
@@ -84,4 +84,26 @@ router.get("/secret", ensureLoggedIn, async function(req, res, next) {
   }
 });
 
+function ensureCorrectUser(req, res, next) {
+  try {
+    const authHeaderValue = req.headers.authorization;
+    const token = jwt.verify(authHeaderValue, SECRET);
+    if (token.username === req.params.username) {
+      return next();
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  } catch (e) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+}
+
+router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+  try {
+    return res.json({ message: "You made it!" });
+  } catch (err) {
+    return res.json(err);
+  }
+});
+
 module.exports = router;
